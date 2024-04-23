19e55ace83cd9bb4815dd9cf9bd7ecba5de5ed03
Mahmoud Khaled <114943206+makh128@users.noreply.github.com>
2024-04-23 13:20:14 +0200

    Change second screen shot text

--- src/component/SecondScreenSh.jsx
+++ src/component/SecondScreenSh.jsx
@@ -7,8 +7,7 @@ function SecondScreenSh() {
           <div className='screen-text d-flex align-items-center flex-column p-5'>
                 <h1 className='py-4 text-light' style={{fontSize:"55px"}}>Expertex Workspace</h1>
               <p className='text-light fs-4'>
-              Expertex Workspace offers AI-powered business building, enabling individuals of
-              any background and experience to launch their standalone AI solution in just 6 minutes.
+              Expertex workspace for AI SaaS products builders, allowing individuals of any background and experience to launch their standalone AI solution within 6 minutes.
               </p>
           </div>
           <div>
